4defaab69f6b7bcc0b238217b0f83d93e17893a6
bitsofbits <tim@bitsofbits.com>
2020-03-08 17:18:06 -0700

    clean up conversion function

--- pyseas/cm.py
+++ pyseas/cm.py
@@ -19,36 +19,45 @@ unofficial.jc_linearblue
 unofficial.jc_linearpink
 """
 from matplotlib import colors
+import numpy as np
 
 
-def _tuple2cmap(name, color_seq):
+def _hex2cmap(name, hex_colors):
+    '''Convert sequence of hex colors to matplotlib cmap
+
+    Parameters
+    ----------
+    name : str
+    hex_colors : sequence of str
+        Must be in '#RRGGBB' format
+
+    Returns
+    -------
+    LinearSegmentedColormap
     '''
-    '''
-    c = color_seq
+    def ramp(i0, i1):
+        float_colors = [(int(x[i0:i1], 16) / 256.0) for x in hex_colors]
+        ramp = zip(np.linspace(0, 1, len(hex_colors), endpoint=True), float_colors, float_colors)
+        return tuple(ramp)
 
-    color_ramp = [ [c[i], int(100*(i/(len(c)-1))), int(100*i/(len(c)-1)) ] for i in range(len(c))]
-    tm = 100.0 #this is just to scale the the following... this makes the colormap
-    cdict = { 'red':tuple(   (color[2]/tm, int(color[0][1:3],16)/256.0, int(color[0][1:3],16)/256.0) 
-                                  for color in color_ramp ),
-              'green':tuple( (color[2]/tm, int(color[0][3:5],16)/256.0, int(color[0][3:5],16)/256.0) 
-                                  for color in color_ramp ),
-              'blue':tuple(  (color[2]/tm, int(color[0][5:7],16)/256.0, int(color[0][5:7],16)/256.0) 
-                                  for color in color_ramp )}#,
+    cdict = { 'red': ramp(1, 3),
+              'green': ramp(3, 5),
+              'blue':  ramp(5, 7) }
 
     cmap = colors.LinearSegmentedColormap(name, cdict, 256)
     cmap.set_bad(alpha = 0.0)
     return cmap
 
 
-reception = _tuple2cmap('reception', ('#ff4573', '#7b2e8d', '#093b76', '#0c276c')) 
-fishing = _tuple2cmap('fishing', ('#0c276c', '#3b9088', '#eeff00', '#ffffff')) 
-presence = _tuple2cmap('presence', ('#0c276c', '#114685','#00ffc3','#ffffff'))
+reception = _hex2cmap('reception', ('#ff4573', '#7b2e8d', '#093b76', '#0c276c')) 
+fishing = _hex2cmap('fishing', ('#0c276c', '#3b9088', '#eeff00', '#ffffff')) 
+presence = _hex2cmap('presence', ('#0c276c', '#114685','#00ffc3','#ffffff'))
 
 class _Unofficial(object):
-  jc_presence  = _tuple2cmap('jc_presence', ('#3359A8', '#16A3A4', '#00FFC3', '#ffffff'))
-  jc_reception = _tuple2cmap('jc_reception', ('#5E0C20', '#2927A8', '#41FFA7', '#ffffff'))
-  jc_linearorange = _tuple2cmap('jc_linearorange', ('#0C276C', '#824158', '#FF9500', '#ffffff'))
-  jc_linearblue = _tuple2cmap('jc_linearblue', ('#0C276C', '#1D5780', '#00FFC3', '#ffffff'))
-  jc_linearpink = _tuple2cmap('jc_linearpink', ('#0C276C', '#4E289B', '#F74559', '#ffffff'))
+  jc_presence  = _hex2cmap('jc_presence', ('#3359A8', '#16A3A4', '#00FFC3', '#ffffff'))
+  jc_reception = _hex2cmap('jc_reception', ('#5E0C20', '#2927A8', '#41FFA7', '#ffffff'))
+  jc_linearorange = _hex2cmap('jc_linearorange', ('#0C276C', '#824158', '#FF9500', '#ffffff'))
+  jc_linearblue = _hex2cmap('jc_linearblue', ('#0C276C', '#1D5780', '#00FFC3', '#ffffff'))
+  jc_linearpink = _hex2cmap('jc_linearpink', ('#0C276C', '#4E289B', '#F74559', '#ffffff'))
 
 unofficial = _Unofficial()
