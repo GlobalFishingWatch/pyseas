5e8533f9f5160e28a20bbc73d3f20bcf96301b7d
Tim Hochberg <tim.hochberg@ieee.org>
2021-06-01 06:26:42 -0700

    cosmetic improvements in colorbar.py

--- pyseas/maps/colorbar.py
+++ pyseas/maps/colorbar.py
@@ -21,11 +21,11 @@ def add_colorbar(img, *, ax=None, fig=None, label='', loc='bottom', width=0.33, 
     format : matplotlib formatter
         Formatter for colorbar labels.
     hspace : float, optional
-        Space between colorbar and axis.
+        Height between colorbar and axis.
     wspace : float, optional
-        Space between colorbar and label.
+        Width between colorbar and label.
     valign : float, optional
-        Alignment of center of text relative to colorbar.
+        Vertical alignment of center of text relative to colorbar.
     right_edge : float, optional
         Location of right edge of colorbar in Axes coords.
 
@@ -40,9 +40,8 @@ def add_colorbar(img, *, ax=None, fig=None, label='', loc='bottom', width=0.33, 
     if right_edge is None:
         is_global = isinstance(core._last_projection, str) and core._last_projection.startswith('global.')
         right_edge = 0.78 if is_global else 1.0
-    padded_height = height + hspace
     wloc = right_edge - width
-    hloc = 0.98 + hspace if (loc == 'top') else -padded_height   
+    hloc = 0.98 + hspace if (loc == 'top') else -(height + hspace)   
 
     cb_ax = ax.inset_axes([wloc, hloc, width, height], transform=ax.transAxes)
     cb = plt.colorbar(img, ax=ax, cax=cb_ax, orientation='horizontal', ticklocation=loc, format=format)
